51f9f726b54009f51b5d7057ee7dc13831fa672b
ITD16 <it.support@d16pro.com>
2026-03-28 05:32:05 +0700

    update background index

--- js/background.js
+++ js/background.js
@@ -1,11 +1,13 @@
 particlesJS("particles-js", {
   particles: {
-    number: { value: 80, density: { enable: true, value_area: 800 } },
-    color: { value: "#939393" },
+    number: {
+      value: 80,
+      density: { enable: true, value_area: 800 },
+    },
+    color: { value: "#8b93a7" },
     shape: {
       type: "circle",
       stroke: { width: 0, color: "#000000" },
-      polygon: { nb_sides: 5 },
     },
     opacity: {
       value: 0.5,
@@ -19,8 +21,8 @@ particlesJS("particles-js", {
     },
     line_linked: {
       enable: true,
-      distance: 135,
-      color: "#989595",
+      distance: 150,
+      color: "#a0a8bb",
       opacity: 0.4,
       width: 1,
     },
@@ -43,9 +45,9 @@ particlesJS("particles-js", {
       resize: true,
     },
     modes: {
-      grab: { distance: 200, line_linked: { opacity: 1 } },
+      grab: { distance: 180, line_linked: { opacity: 0.8 } },
       bubble: { distance: 150, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
+      repulse: { distance: 180, duration: 0.4 },
       push: { particles_nb: 4 },
       remove: { particles_nb: 2 },
     },
