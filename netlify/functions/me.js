24c5905b8ae9670b0b468fb58ee56d933d9751b0
ITD16 <it.support@d16pro.com>
2026-03-31 14:48:57 +0700

    update

--- netlify/functions/me.js
+++ netlify/functions/me.js
@@ -8,6 +8,8 @@ function normalizeAllowedPanels(user) {
       "domains1b",
       "domains789",
       "domains0b",
+      "webscam1b",
+      "webscam0b",
       "vmixConfig",
       "vmixConfig2",
     ];
@@ -21,6 +23,8 @@ function normalizeAllowedPanels(user) {
   if (!!p.domains1b) out.push("domains1b");
   if (!!p.domains789) out.push("domains789");
   if (!!p.domains0b) out.push("domains0b");
+  if (!!p.domains0b) out.push("webscam1b");
+  if (!!p.domains0b) out.push("webscam0b");
   if (!!p.vmixConfig) out.push("vmixConfig");
   if (!!p.vmixConfig2) out.push("vmixConfig2");
 
